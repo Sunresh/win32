f39836b58bdd56e61e594bc86c67d768991f2d6b
Naresh Dhungana <hinareshg@gmail.com>
2023-12-08 10:55:01 +0900

    Add files via upload

--- WindowsProject1.cpp
+++ WindowsProject1.cpp
@@ -7,11 +7,14 @@
 #define ID_BTN_CAMERA_ON 130
 #define ID_BTN_CAMERA_OFF 131
 #define ID_BTN_LASER_ON 132
-bool stopCamera = false;
+bool stopCamera = true;
+double meancv;
 cv::VideoCapture cap;
+std::deque<double> brightData;
 Camera cam;
 HWND g_hFrame1;
 HWND zoomfram;
+HWND graphframe;
 HINSTANCE hInst;
 WCHAR szTitle[MAX_LOADSTRING];
 WCHAR szWindowClass[MAX_LOADSTRING];
@@ -22,6 +25,63 @@ LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 void DisplayCameraFrame(HWND hWnd,HWND hWn);
 void ReleaseCameraResources(HDC hdc);
+
+
+std::atomic<bool> stopGraphUpdate(true);
+void UpdateGraph(HWND graphframe) {
+	while (!stopGraphUpdate) {
+		if (!brightData.empty()) {
+			HDC graphf = GetDC(graphframe);
+			RECT rect;
+			GetClientRect(graphframe, &rect);
+			FillRect(graphf, &rect, (HBRUSH)GetStockObject(WHITE_BRUSH));
+
+			// Drawing code for X-axis (horizontal line at the center)
+			HPEN hXAxisPen = CreatePen(PS_SOLID, 1, RGB(200, 200, 200)); // Adjust color as needed
+			SelectObject(graphf, hXAxisPen);
+			MoveToEx(graphf, rect.left, rect.bottom / 2, NULL);
+			LineTo(graphf, rect.right, rect.bottom / 2);
+
+			// Drawing code for Y-axis (vertical line at the center)
+			MoveToEx(graphf, rect.right / 2, rect.top, NULL);
+			LineTo(graphf, rect.right / 2, rect.bottom);
+
+			// Drawing code for the graph
+			HPEN hGraphPen = CreatePen(PS_SOLID, 1, RGB(0, 0, 255)); // Adjust graph color as needed
+			SelectObject(graphf, hGraphPen);
+
+			int dataCount = brightData.size();
+			double scaleY = 0.8; // Adjust this scale factor as needed
+			int startX = 0;
+
+			for (int x = 0; x < rect.right; ++x) {
+				int dataIndex = static_cast<int>((static_cast<double>(x) / rect.right) * dataCount);
+				double y = brightData[dataIndex] * scaleY + 10; // Adjust additional offsets as needed
+
+				int startY = rect.bottom - static_cast<int>(y);
+				if (x == 0) {
+					MoveToEx(graphf, startX, startY, NULL);
+				}
+				else {
+					LineTo(graphf, x, startY);
+				}
+				startX = x;
+			}
+
+			DeleteObject(hXAxisPen);
+			DeleteObject(hGraphPen);
+			ReleaseDC(graphframe, graphf);
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Adjust the delay as needed
+	}
+}
+
+
+
+
+
+
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -68,7 +128,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 }
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+   hInst = hInstance;
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
    if (!hWnd)
    {
@@ -94,7 +154,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		CreateWindowW(L"BUTTON", L"Deposition OFF", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 2, 175, 255, 30, hFrame, NULL, NULL, NULL);
 		g_hFrame1 = CreateWindowW(L"BUTTON", L"Camera", WS_VISIBLE | WS_CHILD | BS_GROUPBOX, 262, 2, 260, 215, hWnd, NULL, NULL, NULL);
 		zoomfram = CreateWindowW(L"BUTTON", L"ZOOM", WS_VISIBLE | WS_CHILD | BS_GROUPBOX, 522, 2, 260, 215, hWnd, NULL, NULL, NULL);
-		CreateWindowW(L"BUTTON", L"Graph", WS_VISIBLE | WS_CHILD | BS_GROUPBOX, 2, 215, 520, 215, hWnd, NULL, NULL, NULL);
+		graphframe = CreateWindowW(L"BUTTON", L"Graph", WS_VISIBLE | WS_CHILD | BS_GROUPBOX, 2, 215, 520, 215, hWnd, NULL, NULL, NULL);
 
 
 		SetWindowLongPtr(hFrame, GWLP_WNDPROC, (LONG_PTR)WndProc);
@@ -107,12 +167,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             switch (wmId)
             {
 			case ID_BTN_CAMERA_ON:
+			{
 				stopCamera = false;
-				DisplayCameraFrame(g_hFrame1,zoomfram);
+				stopGraphUpdate = false; // Start updating the graph
+				std::thread graphThread(UpdateGraph, graphframe);
+				graphThread.detach(); // Detach the graph thread to run independently
+				DisplayCameraFrame(g_hFrame1, zoomfram);
 				InvalidateRect(hWnd, NULL, TRUE);
+			}
 				break;
 			case ID_BTN_CAMERA_OFF:
+			{
 				stopCamera = true;
+				stopGraphUpdate = true; // Stop updating the graph
+				
+			}
 				break;
 			case ID_BTN_LASER_ON:
 			{
@@ -130,33 +199,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-			if (stopCamera) {
-				RECT rect;
-				GetClientRect(hWnd, &rect);
-				// Define the area for the parabolic graph
-				RECT graphRect = { 522, 220, 782, 480 }; // Adjust the coordinates and size as needed
-				// Fill the background
-				FillRect(hdc, &graphRect, (HBRUSH)(COLOR_WINDOW + 1));
-				// Draw the parabolic graph
-				int x, y;
-				HPEN hPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 255)); // Blue color
-				SelectObject(hdc, hPen);
-				MoveToEx(hdc, graphRect.left, graphRect.bottom, NULL);
-				for (x = graphRect.left; x < graphRect.right; x++)
-				{
-					y = (graphRect.bottom - graphRect.top) - ((x - graphRect.left) * (x - graphRect.right)) / (graphRect.right - graphRect.left);
-					LineTo(hdc, x, y);
-				}
-				DeleteObject(hPen);
-			}
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+
+		EndPaint(hWnd, &ps);
+	}
+	break;
+
+	case WM_DESTROY:
         PostQuitMessage(0);
         break;
     default:
@@ -174,12 +226,12 @@ void DisplayCameraFrame(HWND hWnd, HWND hWn)
 	HDC hdc = GetDC(hWnd); // Obtain device context for the window
 	HDC hdc1 = GetDC(hWn); // Obtain device context for the window
 
-	cv::Mat frame;
+	cv::Mat dframe,frame;
 	while (!stopCamera) {
-		cap >> frame;
-		if (!frame.empty()) {
+		cap >> dframe;
+		if (!dframe.empty()) {
+			cv::flip(dframe, frame, 1);//flip
 			cv::resize(frame, frame, cv::Size(260, 215));
-
 			// Show original frame in g_hFrame1
 			cv::Mat tmpFrameOriginal;
 			cv::cvtColor(frame, tmpFrameOriginal, cv::COLOR_BGR2BGRA);
@@ -189,7 +241,10 @@ void DisplayCameraFrame(HWND hWnd, HWND hWn)
 			cv::Mat tmpFrameCropped;
 			cv::cvtColor(croppedFrame, tmpFrameCropped, cv::COLOR_BGR2BGRA);
 			cv::resize(tmpFrameCropped, tmpFrameCropped, cv::Size(260, 215));
-
+			meancv = cam.meanofBri(tmpFrameCropped);
+			brightData.push_back(meancv);
+			std::wstring meanString = std::to_wstring(meancv);
+			OutputDebugStringW(meanString.c_str());
 			// Display original frame in g_hFrame1
 			HBITMAP hBitmapOriginal = CreateBitmap(tmpFrameOriginal.cols, tmpFrameOriginal.rows, 1, 32, tmpFrameOriginal.data);
 			HDC memDCOriginal = CreateCompatibleDC(hdc);
@@ -215,48 +270,6 @@ void DisplayCameraFrame(HWND hWnd, HWND hWn)
 	ReleaseCameraResources(hdc);
 }
 
-void DisplayCameraFrameoo(HWND hWnd,HWND hWn)
-{
-	cap.open(0);
-	if (!cap.isOpened()) {
-		return;
-	}
-	HDC hdc = GetDC(hWnd); // Obtain device context for the window
-	HDC hdc1 = GetDC(hWn); // Obtain device context for the window
-
-	cv::Mat frame;
-	while (!stopCamera) {
-		cap >> frame;
-		if (!frame.empty()) {
-			cv::resize(frame, frame, cv::Size(260, 215));
-			cv::Mat tmpFrame;
-			//cv::cvtColor(frame, tmpFrame, cv::COLOR_BGR2BGRA);
-			cv::cvtColor(frame, tmpFrame, cv::COLOR_BGR2BGRA);
-
-			HBITMAP hBitmap = CreateBitmap(tmpFrame.cols, tmpFrame.rows, 1, 32, tmpFrame.data);
-			HDC memDC = CreateCompatibleDC(hdc);
-			HDC memDC1 = CreateCompatibleDC(hdc1);
-
-			SelectObject(memDC, hBitmap);
-			SelectObject(memDC1, hBitmap);
-
-			BITMAP bm;
-			GetObject(hBitmap, sizeof(bm), &bm);
-			BitBlt(hdc, 0, 0, bm.bmWidth, bm.bmHeight, memDC, 0, 0, SRCCOPY);
-			BitBlt(hdc1, 0, 0, bm.bmWidth, bm.bmHeight, memDC1, 0, 0, SRCCOPY);
-
-			DeleteDC(memDC);
-			DeleteDC(memDC1);
-			DeleteObject(hBitmap);
-		}
-		InvalidateRect(hWnd, NULL, TRUE);
-		if (cv::waitKey(0) == 'q') {
-			stopCamera = true;
-			break;
-		}
-	}
-	ReleaseCameraResources(hdc);
-}
 void ReleaseCameraResources(HDC hdc) {
 	cap.release();
 	ReleaseDC(g_hFrame1, hdc); // Release the device context
--- camera.cpp
+++ camera.cpp
@@ -16,6 +16,15 @@ public:
 	void drawRectangle(cv::Mat& frame, int x1, int y1, int x2, int y2, const cv::Scalar& color, int thickness=1) {
 		cv::rectangle(frame, cv::Point(x1, y1), cv::Point(x2, y2), color, thickness);
 	}
+	double meanofBri(cv::Mat& iframe) {
+		cv::Mat grayFrame;
+		cv::cvtColor(iframe, grayFrame, cv::COLOR_BGR2GRAY);
+		double width = grayFrame.cols;
+		double height = grayFrame.rows;
+		double variance = 0.0;
+		double meanCV = cv::mean(grayFrame)[0];
+		return meanCV;
+	}
 
 
 };
